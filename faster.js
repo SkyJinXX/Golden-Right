cacdfa0a127ce94f6ad3b3b7d92634405d8559ba
SkyJin <skyjin@outlook.com>
2020-03-29 00:55:46 +0800

    fix/b站多p播放失效的bug

--- faster.js
+++ faster.js
@@ -2,7 +2,7 @@
 // @name         黄金右键
 // @description  按住右键→倍速播放, 松开右键→恢复原样, 灵活追剧看视频~ 支持b站、YouTube、优酷...
 // @namespace    http://tampermonkey.net/
-// @version      0.10
+// @version      0.2.0
 // @author       SkyJin
 // @include    https://www.bilibili.com/*
 // @include    https://www.youtube.com/*
@@ -15,6 +15,11 @@
     'use strict';
 
     let down_count = 0
+    const faster_rate = 3
+    let normal_rate = 1
+    const add_time = 7
+    let page_video
+
     const tryPageVideo = () => Array.prototype.find.call(document.getElementsByTagName('video'), e => e.offsetWidth > 9)
     const getPageVideo = () => {
         return new Promise(resolve => {
@@ -24,36 +29,43 @@
                     clearInterval(timer)
                     resolve(page_video)
                 }
-            }, 300);
+            }, 500);
         })
     };
-    const page_video = await getPageVideo()
-    const faster_rate = 3
-    let normal_rate = 1
-    const add_time = 7
-    const downEvent = e => {
+    const checkPageVideo = async () => {
+        if (page_video.offsetWidth <= 9) {
+            page_video = await getPageVideo()
+            if (page_video.offsetWidth <= 9) {
+                console.error('播放器检查失败，请检查当前页面是否存在html5播放器')
+                return false
+            }
+        }
+        return true
+    }
+    const downEvent = async e => {
         if (e.keyCode !== 39) return
         e.stopPropagation()
-
-        if (++down_count === 2) {
+        
+        if (++down_count === 2 && await checkPageVideo()) {
             normal_rate = page_video.playbackRate
             page_video.playbackRate = faster_rate
             console.log('加速播放中')
         }
     }
-    const upEvent = e => {
+    const upEvent = async e => {
         if (e.keyCode !== 39) return
         e.stopPropagation()
-
-        if (down_count === 1) {
+        
+        if (down_count === 1 && await checkPageVideo()) {
             page_video.currentTime += add_time
             console.log('前进' + add_time + '秒')
         }
-
+        
         down_count = 0
         page_video.playbackRate = normal_rate
     }
-
+    
+    page_video = await getPageVideo()
     document.body.addEventListener('keydown', downEvent, true)
     document.body.parentElement.addEventListener('keyup', upEvent, true)
 
