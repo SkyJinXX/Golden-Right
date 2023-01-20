0ab5db4467667cbcc287ee0a16f2698c2c85654e
SkyJin <skyjin@outlook.com>
2023-01-20 22:42:31 +0800

    Support native control for YouTube (speed up the subtitle too!)

--- faster.js
+++ faster.js
@@ -2,7 +2,7 @@
 // @name         黄金右键-Golden Right
 // @description  按住"→"键倍速播放, 松开"→"键恢复原样, 灵活追剧看视频~ 支持所有H5视频的网站（YouTube、腾讯视频、优酷、番剧等）
 // @namespace    http://tampermonkey.net/
-// @version      1.0.7
+// @version      1.0.8
 // @author       SkyJin
 // @include     http://*
 // @include     https://*
@@ -29,6 +29,24 @@
         }
         return Array.prototype.slice.call(arr);
     }
+    const init = () => {
+        if (location.origin.indexOf("youtube.com") > -1) {
+            document.body.addEventListener("keydown", downEvent_YT, true);
+            document.body.parentElement.addEventListener("keyup", upEvent_YT, true);
+        } else {
+            document.body.addEventListener("keydown", downEvent, true);
+            document.body.parentElement.addEventListener("keyup", upEvent, true);
+        }
+    };
+/*
+.............########..#######..########......#######..########.##.....##.########.########...######.
+.............##.......##.....##.##.....##....##.....##....##....##.....##.##.......##.....##.##....##
+.............##.......##.....##.##.....##....##.....##....##....##.....##.##.......##.....##.##......
+.............######...##.....##.########.....##.....##....##....#########.######...########...######.
+.............##.......##.....##.##...##......##.....##....##....##.....##.##.......##...##.........##
+.............##.......##.....##.##....##.....##.....##....##....##.....##.##.......##....##..##....##
+.............##........#######..##.....##.....#######.....##....##.....##.########.##.....##..######.
+*/
     const getPageVideo = () => {
         console.log("Finding available Video Element...");
         const allVideoElementArray = makeArray(
@@ -43,7 +61,7 @@
         );
 
         if (page_video) {
-            console.log("Found the Viedeo Element!");
+            console.log("Found the Video Element!");
             return page_video;
         } else {
             console.log("找不到正在播放的Video Element");
@@ -92,7 +110,7 @@
                 relativeEvent.shouldPrevent && relativeEvent.prevent();
                 normal_rate = page_video.playbackRate;
                 page_video.playbackRate = faster_rate;
-                console.log("加速播放中");
+                console.log("加速播放中...");
             }
         }
     };
@@ -117,10 +135,80 @@
         // 计数-重置
         down_count = 0;
     };
-    const init = () => {
-        document.body.addEventListener("keydown", downEvent, true);
-        document.body.parentElement.addEventListener("keyup", upEvent, true);
-    };
+/*
+    .########..#######..########.....##....##..#######..##.....##.########.##.....##.########..########
+    .##.......##.....##.##.....##.....##..##..##.....##.##.....##....##....##.....##.##.....##.##......
+    .##.......##.....##.##.....##......####...##.....##.##.....##....##....##.....##.##.....##.##......
+    .######...##.....##.########........##....##.....##.##.....##....##....##.....##.########..######..
+    .##.......##.....##.##...##.........##....##.....##.##.....##....##....##.....##.##.....##.##......
+    .##.......##.....##.##....##........##....##.....##.##.....##....##....##.....##.##.....##.##......
+    .##........#######..##.....##.......##.....#######...#######.....##.....#######..########..########
+*/
+    const getPageVideo_YT = () => {
+        console.log("Finding available Video Element...");
+        let pv;
 
+        if (document.getElementById("ytd-player") && checkPageVideo_YT(document.getElementById("ytd-player").player_))
+            pv = document.getElementById("ytd-player").player_
+
+        if (pv) {
+            console.log("Found the Video Element!");
+            return pv;
+        } else {
+            console.log("找不到正在播放的Video Element");
+        }
+    };
+    const checkPageVideo_YT = (v) => {
+        if (v) {
+            return v.getPlayerState() == 1;
+        } else {
+            return false;
+        }
+    };
+    const downEvent_YT = (e) => {
+        if (e.keyCode !== 39) return;
+        e.stopPropagation();
+
+        // 计数+1
+        down_count++;
+
+        // 长按右键-开始
+        if (down_count === 2) {
+            if (checkPageVideo_YT(page_video) || (page_video = getPageVideo_YT())) {
+                normal_rate = page_video.getPlaybackRate();
+                page_video.setPlaybackRate(faster_rate);
+                console.log("加速播放中...");
+            }
+        }
+    };
+    const upEvent_YT = (e) => {
+        if (e.keyCode !== 39) return;
+        e.stopPropagation();
+
+        // 单击右键时
+        if (down_count === 1) {
+            if (checkPageVideo_YT(page_video) || (page_video = getPageVideo_YT())) {
+                page_video.seekToStreamTime(page_video.getCurrentTime() + add_time);
+                console.log("前进" + add_time + "秒");
+            }
+        }
+
+        // 长按右键-结束
+        if (page_video && page_video.playbackRate !== normal_rate) {
+            page_video.setPlaybackRate(normal_rate);
+        }
+
+        // 计数-重置
+        down_count = 0;
+    };
+/*
+.............####.##....##.####.########.####....###....########.########
+..............##..###...##..##.....##.....##....##.##......##....##......
+..............##..####..##..##.....##.....##...##...##.....##....##......
+..............##..##.##.##..##.....##.....##..##.....##....##....######..
+..............##..##..####..##.....##.....##..#########....##....##......
+..............##..##...###..##.....##.....##..##.....##....##....##......
+.............####.##....##.####....##....####.##.....##....##....########
+*/
     init();
 })();
